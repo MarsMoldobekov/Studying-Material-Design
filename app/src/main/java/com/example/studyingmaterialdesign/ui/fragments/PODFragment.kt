481271d7cb4f0f25f3e355046682a112db06fa2d
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-09-26 13:39:56 +0300

    BlackAndWhite Theme implemented

--- app/src/main/java/com/example/studyingmaterialdesign/ui/fragments/PODFragment.kt
+++ app/src/main/java/com/example/studyingmaterialdesign/ui/fragments/PODFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
@@ -16,21 +17,22 @@ import com.example.studyingmaterialdesign.R
 import com.example.studyingmaterialdesign.databinding.FragmentPodBinding
 import com.example.studyingmaterialdesign.viewmodel.PODData
 import com.example.studyingmaterialdesign.viewmodel.ViewModel
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.snackbar.Snackbar
 import java.time.LocalDate
+import java.util.*
 
-//TODO(add chipGroup for hdurl and url, delete tomorrow chip)
+//TODO(add chipGroup for hdurl and url in settings)
 class PODFragment : Fragment() {
-    companion object {
-        fun newInstance(): Fragment = PODFragment()
-    }
-
     private var _binding: FragmentPodBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: ViewModel by lazy {
         ViewModelProvider(this).get(ViewModel::class.java)
     }
+
+    private val onCheckedChangeListener = OnCheckedChangeListener()
+    private var day = Days.TODAY
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,38 +48,20 @@ class PODFragment : Fragment() {
 
         with(binding) {
             chipGroup.check(R.id.chip_today)
-            chipGroup.setOnCheckedChangeListener { _, checkedId ->
-                when (checkedId) {
-                    R.id.chip_yesterday -> {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            viewModel.load(LocalDate.now().minusDays(1).toString())
-                        }
-                    }
-                    R.id.chip_today -> {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            viewModel.load(LocalDate.now().toString())
-                        }
-                    }
-                    R.id.chip_tomorrow -> {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            viewModel.load(LocalDate.now().toString())
-                        }
-                    }
-                }
-            }
+            chipGroup.setOnCheckedChangeListener(onCheckedChangeListener)
             textInputLayout.setEndIconOnClickListener {
                 startActivityActionView(BuildConfig.WIKI_BASE_URL + binding.textInputEditText.text.toString())
             }
             buttonNasa.setOnClickListener { startActivityActionView(BuildConfig.NASA_OFFICIAL_SITE) }
             bar.replaceMenu(R.menu.menu_bottom_app_bar)
-            bar.setOnMenuItemClickListener OnMenuItemClickListener@{ menuItem ->
+            bar.setOnMenuItemClickListener { menuItem ->
                 when (menuItem.itemId) {
                     R.id.app_bar_settings -> Toast.makeText(context, "Settings", Toast.LENGTH_SHORT)
                         .show()
                     R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT)
                         .show()
                 }
-                return@OnMenuItemClickListener true
+                return@setOnMenuItemClickListener true
             }
         }
 
@@ -104,7 +88,11 @@ class PODFragment : Fragment() {
                 binding.loadingLayout.visibility = View.GONE
                 binding.imageView.setImageResource(R.drawable.ic_no_photo_vector)
                 Snackbar.make(binding.root, podData.throwable.toString(), Snackbar.LENGTH_LONG)
-                    .setAction("RE-LOAD") { /*viewModel.load()*/ }.show()
+                    .setAction("RE-LOAD") {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            onCheckedChangeListener.loadByDay()
+                        }
+                    }.show()
             }
             PODData.Loading -> {
                 binding.loadingLayout.visibility = View.VISIBLE
@@ -114,6 +102,35 @@ class PODFragment : Fragment() {
                 podData.podResponse.url?.let { binding.imageView.load(it) }
                 podData.podResponse.title?.let { binding.title.text = it }
                 podData.podResponse.explanation?.let { binding.explanation.text = it }
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance(): Fragment = PODFragment()
+    }
+
+    private enum class Days { YESTERDAY, TODAY }
+
+    private inner class OnCheckedChangeListener : ChipGroup.OnCheckedChangeListener {
+        override fun onCheckedChanged(group: ChipGroup?, checkedId: Int) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                day = when (checkedId) {
+                    R.id.chip_yesterday -> Days.YESTERDAY
+                    R.id.chip_today -> Days.TODAY
+                    else -> day //CRUTCH
+                }
+                loadByDay()
+            } else {
+                //TODO(load date if Build.VERSION.SDK_INT <= Build.VERSION_CODES.O)
+            }
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        fun loadByDay() {
+            when (day) {
+                Days.YESTERDAY -> viewModel.load(LocalDate.now().minusDays(1).toString())
+                Days.TODAY -> viewModel.load(LocalDate.now().toString())
             }
         }
     }
